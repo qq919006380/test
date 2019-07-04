9db2d7b935b6d8b8d368fcc9b829eafb61d2dfb1
weibaichao <919006380@qq.com>
2019-07-04 10:24:33 +0800

    初步完成选择周高亮ffffff

--- app.vue
+++ app.vue
@@ -1,16 +1,55 @@
 <template>
   <div class="cotent">
-    <DatePicker id='pick_week' type="date" placeholder="Select date" style="width: 200px"></DatePicker>
+    <DatePicker @on-change='on'	 @on-open-change='change' id='pick_week' type="date" placeholder="Select date" style="width: 200px"></DatePicker>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods:{
+    on(){
+      console.log('asd')
+    },
+    change(){
+        for(var i=1;i<=7;i++){
+          document.querySelectorAll('#pick_week>.ivu-select-dropdown .ivu-date-picker-cells .ivu-date-picker-cells-cell')[i].onmouseenter=()=>{        
+          this.addClass(1,7)
+          }
+          document.querySelectorAll('#pick_week>.ivu-select-dropdown .ivu-date-picker-cells .ivu-date-picker-cells-cell')[i].onmouseleave =()=>{        
+            this.clearClass(1,7)
+          }
+        }
+
+        for(var i=7;i<=14;i++){
+          document.querySelectorAll('#pick_week>.ivu-select-dropdown .ivu-date-picker-cells .ivu-date-picker-cells-cell')[i].onmouseenter=()=>{        
+          this.addClass(8,14)
+          }
+          document.querySelectorAll('#pick_week>.ivu-select-dropdown .ivu-date-picker-cells .ivu-date-picker-cells-cell')[i].onmouseleave =()=>{        
+            this.clearClass(8,14)
+          }
+        }
+    },
+    addClass(a,b){
+      for(let i=a;i<=b;i++){
+        document.querySelectorAll('#pick_week .ivu-date-picker-cells-cell')[i].classList.add('add')
+      }
+    },
+    clearClass(a,b){
+      for(let i=a;i<=b;i++){
+        document.querySelectorAll('#pick_week .ivu-date-picker-cells-cell')[i].classList.remove('add')
+      }
+    }
+  },
+  mounted(){
+    
+  }
+};
 </script>
 
-<style scoped lang='less'>
-  .content{
-    text-align: center;
-    #pick_week{}
+<style lang='less'>
+  #pick_week{
+    .ivu-date-picker-cells-cell.add{
+      background: #e1f0fe;
+    }
   }
 </style>
