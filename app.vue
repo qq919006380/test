36a09d8ea7e62cd953d7585ff2b1a5a871b6e8ac
weibaichao <919006380@qq.com>
2019-07-05 09:23:21 +0800

    完善日期组件

--- app.vue
+++ app.vue
@@ -1,55 +1,90 @@
 <template>
-  <div class="cotent">
-    <DatePicker @on-change='on'	 @on-open-change='change' id='pick_week' type="date" placeholder="Select date" style="width: 200px"></DatePicker>
+  <div class="cotent" @click="change">
+    <DatePicker
+      @on-open-change="change"
+      id="pick_week"
+      type="date"
+      placeholder="Select date"
+      style="width: 200px"
+    ></DatePicker>
   </div>
 </template>
 
 <script>
 export default {
-  methods:{
-    on(){
-      console.log('asd')
-    },
-    change(){
-        for(var i=1;i<=7;i++){
-          document.querySelectorAll('#pick_week>.ivu-select-dropdown .ivu-date-picker-cells .ivu-date-picker-cells-cell')[i].onmouseenter=()=>{        
-          this.addClass(1,7)
-          }
-          document.querySelectorAll('#pick_week>.ivu-select-dropdown .ivu-date-picker-cells .ivu-date-picker-cells-cell')[i].onmouseleave =()=>{        
-            this.clearClass(1,7)
-          }
-        }
+  methods: {
+    change() {
+      let em = document.querySelectorAll(
+        "#pick_week>.ivu-select-dropdown .ivu-date-picker-cells .ivu-date-picker-cells-cell"
+      );
+      for (let i = 1; i <= 7; i++) {
+        em[i].onmouseenter = () => {
+          this.addClass(1, 7);
+        };
+        em[i].onmouseleave = () => {
+          this.clearClass(1, 7);
+        };
+      }
 
-        for(var i=7;i<=14;i++){
-          document.querySelectorAll('#pick_week>.ivu-select-dropdown .ivu-date-picker-cells .ivu-date-picker-cells-cell')[i].onmouseenter=()=>{        
-          this.addClass(8,14)
-          }
-          document.querySelectorAll('#pick_week>.ivu-select-dropdown .ivu-date-picker-cells .ivu-date-picker-cells-cell')[i].onmouseleave =()=>{        
-            this.clearClass(8,14)
-          }
-        }
-    },
-    addClass(a,b){
-      for(let i=a;i<=b;i++){
-        document.querySelectorAll('#pick_week .ivu-date-picker-cells-cell')[i].classList.add('add')
+      for (let i = 8; i <= 14; i++) {
+        em[i].onmouseenter = () => {
+          this.addClass(8, 14);
+        };
+        em[i].onmouseleave = () => {
+          this.clearClass(8, 14);
+        };
+      }
+
+      for (let i = 15; i <= 21; i++) {
+        em[i].onmouseenter = () => {
+          this.addClass(15, 21);
+        };
+        em[i].onmouseleave = () => {
+          this.clearClass(15, 21);
+        };
+      }
+
+      for (let i = 22; i <= 28; i++) {
+        em[i].onmouseenter = () => {
+          this.addClass(22, 28);
+        };
+        em[i].onmouseleave = () => {
+          this.clearClass(22, 28);
+        };
+      }
+
+      for (let i = 29; i <= 35; i++) {
+        em[i].onmouseenter = () => {
+          this.addClass(29, 35);
+        };
+        em[i].onmouseleave = () => {
+          this.clearClass(29, 35);
+        };
       }
     },
-    clearClass(a,b){
-      for(let i=a;i<=b;i++){
-        document.querySelectorAll('#pick_week .ivu-date-picker-cells-cell')[i].classList.remove('add')
+    addClass(a, b) {
+      for (let i = a; i <= b; i++) {
+        document
+          .querySelectorAll("#pick_week .ivu-date-picker-cells-cell")
+          [i].classList.add("add");
+      }
+    },
+    clearClass(a, b) {
+      for (let i = a; i <= b; i++) {
+        document
+          .querySelectorAll("#pick_week .ivu-date-picker-cells-cell")
+          [i].classList.remove("add");
       }
     }
   },
-  mounted(){
-    
-  }
+  mounted() {}
 };
 </script>
 
 <style lang='less'>
-  #pick_week{
-    .ivu-date-picker-cells-cell.add{
-      background: #e1f0fe;
-    }
+#pick_week {
+  .ivu-date-picker-cells-cell.add {
+    background: #e1f0fe;
   }
+}
 </style>
