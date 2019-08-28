eb65e69c045e88da21afd8688510030cec39445c
weibo <919006380@qq.com>
2019-08-28 20:44:32 +0800

    update

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,8 +1,8 @@
 export default [
-    { path: '/', name: "默认", redirect: 'pop' },
+    { path: '/', name: "默认", redirect: 'list' },
     { path: '/list', name: '列表', component: () => import('~/view/list.vue') },
     { path: '/week', name: '选择周', component: () => import('~/view/week.vue') },
-    { path: '/pop', name: 'pop', component: () => import('~/view/pop.vue') },
+    { path: '/props', name: 'props', component: () => import('~/view/pop.vue') },
     { path: '/initData', name: '初始化data', component: () => import('~/view/initData.vue') },
     { path: '/orderTable', name: '订单table', component: () => import('~/view/orderTable/index.vue') },
     { path: '/selectTable', name: '多选&单选table', component: () => import('~/view/selectTable.vue') },
