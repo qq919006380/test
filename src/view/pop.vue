7754ff5a06f30c28ad2738a4bec498d0454ec1b2
weibo <919006380@qq.com>
2020-01-01 19:05:16 +0800

    更新引用类型刷新页面不刷新的坑

--- src/view/pop.vue
+++ src/view/pop.vue
@@ -1,24 +1,32 @@
 <template>
-  <div>{{obj}}</div>
+  <div>
+    <div>{{obj}}</div>
+    <div>{{arr}}</div>
+  </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      obj:{
-        a:"aa",
-        b:"bb"
-      }
-    }
+      obj: {
+        a: "aa",
+        b: "bb"
+      },
+      arr: ["a", "b"]
+    };
   },
-  mounted(){
-    setTimeout(()=>{
-      // this.obj.c='xxx'
-      this.$set(this.obj,'c','xxx')
-    },800)
+  mounted() {
+    setTimeout(() => {
+      /**obj error */
+      // Vue 不能检测对象属性的添加或删除
+      this.obj.c = "xxx";
+      // this.$set(this.obj, "c", "xxx");
+      /**arr error*/
+      this.arr[0] = "我发生了变化";
+      this.arr.length=0//除非用arr.splice(0)
+    }, 800);
   }
- 
 };
 </script>
 
