ac61cd5a969fcd8d7f73b08920eeb0dbbfab0c17
weibo <919006380@qq.com>
2020-08-29 14:51:42 +0800

    add

--- src/view/flex.vue
+++ src/view/flex.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <h2>例子一</h2>
+    <h2>例子一:列数固定</h2>
     <div class="demo1">
-        <div class="item" v-for="item in 7"></div>
+        <div class="item" v-for="item in 35"></div>
     </div>
 
-    <h2>例子二</h2>
-    <div class="demo2"></div>
+    <h2>例子二：子项宽度不固定</h2>
+    <div class="demo2" >
+      <div class="item" v-for="item in 35" :style="{width:item*8+'px'}">{{item}}</div>
+    </div>
   </div>
 </template>
 
@@ -16,18 +18,32 @@ export default {};
 
 <style scoped lang='less'>
 .demo1 {
-//   width: 300px;
   background: pink;
   display: flex;
   flex-wrap: wrap;
   .item {
-    width: 24%;
+    width: 18%;
     height: 100px;
     background-color: skyblue;
     margin-top: 15px;
   }
-  .item:not(:nth-child(4n)) {
-    margin-right: calc(4% / 3);
+  .item:not(:nth-child(5n)) {
+    margin-right: calc(10% / 4);
+    background: purple;
+  }
+}
+.demo2{
+  background: pink;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  .item{
+    height: 100px;
+    background: rebeccapurple;
+    margin: 15px;
+  }
+  .item:last-child{
+    margin-right: auto;
   }
 }
 </style>
