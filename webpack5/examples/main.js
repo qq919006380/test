69b5b65e6a572c865681ff31769cfb005a985b8f
weibo <919006380@qq.com>
2021-05-12 23:03:10 +0800

    add webpack5

---
+++ webpack5/examples/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import xui from '../packages'
+
+const app = createApp(App)
+app.use(xui)
+app.use(router)
+app.mount('#app')
---
+++ webpack5/examples/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <Button type="primary">button</Button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
