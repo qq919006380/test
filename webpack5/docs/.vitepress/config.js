a7d53fa9ba25d5a53aba25ef7a0a4d34b19d61e3
weibo <919006380@qq.com>
2021-05-29 17:50:33 +0800

    添加文档

---
+++ webpack5/docs/.vitepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    title: "pencil-vue",
+    description: "一个手绘风格的UI框架",
+    head: [["link", { rel: "icon", href: "/logo.jpg" }]],
+    themeConfig: {
+      logo: "/logo.jpg",
+      repo: "qq919006380/pencil-vue", // 添加 github 链接
+    }
+  }
