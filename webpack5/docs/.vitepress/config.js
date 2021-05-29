dd0917ca7b28e8dae20b30614720067319a73fd6
weibo <919006380@qq.com>
2021-05-29 19:20:04 +0800

    初步弄好文档结构

--- webpack5/docs/.vitepress/config.js
+++ webpack5/docs/.vitepress/config.js
@@ -1,9 +1,29 @@
 module.exports = {
-    title: "pencil-vue",
-    description: "一个手绘风格的UI框架",
-    head: [["link", { rel: "icon", href: "/logo.jpg" }]],
-    themeConfig: {
-      logo: "/logo.jpg",
-      repo: "qq919006380/pencil-vue", // 添加 github 链接
-    }
+  title: "pencil-vue",
+  description: "一个手绘风格的UI框架",
+  head: [["link", { rel: "icon", href: "/logo.jpg" }]],
+  themeConfig: {
+    logo: "/logo.jpg",
+    repo: "qq919006380/pencil-vue", // 添加 github 链接
+    nav: [{ text: "Home", link: "/index" }],
+    sidebar: {
+      '/': [
+        {
+          text: '入门',
+          children: [
+            { text: '安装', link: '/install/index' },
+            { text: '安装', link: '/install/readem' },
+          ],
+        },
+        {
+          text: '组件',
+          children: [
+            { text: '按钮', link: '/components/button' },
+            { text: '输入框', link: '/components/input' },
+            { text: '卡片', link: '/components/card' },
+          ],
+        },
+      ],
+    },
   }
+}
--- webpack5/docs/README.md
+++ webpack5/docs/README.md
@@ -1,6 +1,6 @@
 ---
-home: true
-heroImage: /logo.jpg
+# home: true
+# heroImage: /logo.jpg
 
 # navbar: false
 # sidebar: auto
