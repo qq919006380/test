3c89a68bf854ae4b0d51b92d4371551e05756d67
weibaichao <weibaichao@ysstech.com>
2021-11-01 10:33:49 +0800

    添加字段拖拽

--- vue2-test/src/components/Count.vue
+++ vue2-test/src/components/Count.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="er-template" ref="tp" @drop="onDragenter" @dragover.prevent >
+  <div class="er-template" ref="tp" @drop="onDragenter" @dragover.prevent>
     <p class="title">
-      <span class="table_chnname" :title="nodeInfo.chnname">{{ nodeInfo.chnname }}</span>(
-      <span class="table_name">{{ nodeInfo.name }}</span>)
+      <span class="table_chnname" :title="nodeInfo.chnname">{{
+        nodeInfo.chnname
+      }}</span
+      >( <span class="table_name">{{ nodeInfo.name }}</span
+      >)
     </p>
     <div class="fileds_area" v-for="item in fieldTable">
-      <div :style="{ width: maxWidth.PK_field + 'px' }">{{ item.PK_field }}</div>
-      <div :style="{ width: maxWidth.normal_field + 'px' }">{{ item.normal_field }}</div>
+      <div :style="{ width: maxWidth.PK_field + 'px' }">
+        {{ item.PK_field }}
+      </div>
+      <div :style="{ width: maxWidth.normal_field + 'px' }">
+        {{ item.normal_field }}
+      </div>
     </div>
   </div>
 </template>
@@ -18,59 +25,73 @@ export default {
     return {
       nodeInfo: { chnname: "", name: "" },
       fieldTable: [
-        { PK_field: "INSTRUCT_ID", normal_field: '授课号' },
-        { PK_field: "CLASS_ID", normal_field: '班级ID' },
-        { PK_field: "LESSON_ID", normal_field: '课程号' },
-        { PK_field: "TEACHER_ID", normal_field: '教师ID' },
-        { PK_field: "TENANT_ID", normal_field: '租户号' },
-        { PK_field: "REVISION", normal_field: '乐观锁' },
-        { PK_field: "CREATED_BY", normal_field: '创建人' },
-        { PK_field: "CREATED_TIME", normal_field: '创建时间' },
-        { PK_field: "UPDATED_BY", normal_field: '更新人' },
+        { PK_field: "INSTRUCT_ID", normal_field: "授课号" },
+        { PK_field: "CLASS_ID", normal_field: "班级ID" },
+        { PK_field: "LESSON_ID", normal_field: "课程号" },
+        { PK_field: "TEACHER_ID", normal_field: "教师ID" },
+        { PK_field: "TENANT_ID", normal_field: "租户号" },
+        { PK_field: "REVISION", normal_field: "乐观锁" },
+        { PK_field: "CREATED_BY", normal_field: "创建人" },
+        { PK_field: "CREATED_TIME", normal_field: "创建时间" },
+        { PK_field: "UPDATED_BY", normal_field: "更新人" },
       ],
       maxWidth: {
-        totalWidth: 0
+        totalWidth: 0,
       },
-    }
+    };
   },
   mounted() {
-    const node = this.getNode()
-    const data = node.store.data.data
+    const node = this.getNode();
+    const data = node.store.data.data;
     if (data) {
-      this.nodeInfo = data.nodeInfo
+      this.nodeInfo = data.nodeInfo;
     }
-
 
     for (var k in this.fieldTable[0]) {
-      let w = Math.max(...this.fieldTable.map(item => this.getTextWidth(item[k])))
-      this.maxWidth[k] = w
-      this.maxWidth.totalWidth = this.maxWidth.totalWidth + w
+      let w = Math.max(
+        ...this.fieldTable.map((item) => this.getTextWidth(item[k]))
+      );
+      this.maxWidth[k] = w;
     }
-
+    
     this.$nextTick(() => {
-      let height = this.$refs.tp.clientHeight
-      let width = this.$refs.tp.clientWidth
-      this.getNode().resize(width, height)
-    })
+      this.updateTableSize();
+    });
   },
   methods: {
+    updateTableSize() {
+      for (var k in this.fieldTable[0]) {
+        let w = Math.max(
+          ...this.fieldTable.map((item) => this.getTextWidth(item[k]))
+        );
+        this.maxWidth[k] = w;
+      }
+
+      this.$nextTick(() => {
+        let height = this.$refs.tp.clientHeight;
+        let width = this.$refs.tp.clientWidth;
+        this.getNode().resize(width, height);
+      });
+    },
+    /**
+     * 根据字符串长度计算出元素的px像素宽度
+     */
     getTextWidth(str, fontSize = 12) {
-      let result
-      let ele = document.createElement('span')
-      //字符串中带有换行符时，会被自动转换成<br/>标签，若需要考虑这种情况，可以替换成空格，以获取正确的宽度
-      //str = str.replace(/\\n/g,' ').replace(/\\r/g,' ');
+      let result;
+      let ele = document.createElement("span");
       ele.innerText = str;
-      //不同的大小和不同的字体都会导致渲染出来的字符串宽度变化，可以传入尽可能完备的样式信息
-      ele.style.fontSize = fontSize + 'px';
-      //由于父节点的样式会影响子节点，这里可按需添加到指定节点上
+      ele.style.fontSize = fontSize + "px";
       document.documentElement.append(ele);
       result = ele.offsetWidth;
       document.documentElement.removeChild(ele);
       return result;
     },
     onDragenter(e) {
-      console.log(e);
-    }
+      let data = JSON.parse(e.dataTransfer.getData("data-info"));
+      console.log(data);
+      this.fieldTable.push(data);
+      this.updateTableSize();
+    },
   },
   inject: ["getGraph", "getNode"],
 };
--- vue2-test/src/view/er/tree-field.vue
+++ vue2-test/src/view/er/tree-field.vue
@@ -1,65 +1,73 @@
 <template>
-    <div>
-        <el-tree default-expand-all :data="data" :props="defaultProps">
-            <span slot-scope="{ node, data }">
-                <!-- @mousedown="node.isLeaf ? handleNodeMove(data, node, $event) : null" -->
-                <span draggable="true" @dragstart='onDragstart' :class="{ move: node.isLeaf }">{{ node.label }}</span>
-            </span>
-        </el-tree>
-    </div>
+  <div>
+    <el-tree default-expand-all :data="data" :props="defaultProps">
+      <span slot-scope="{ node, data }">
+        <!-- @mousedown="node.isLeaf ? handleNodeMove(data, node, $event) : null" -->
+        <span
+          draggable="true"
+          @dragstart="onDragstart($event, data)"
+          :class="{ move: node.isLeaf }"
+          >{{ node.label }}</span
+        >
+      </span>
+    </el-tree>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            data: [
-                {
-                    label: '一级 1',
-                    children: [
-                        { label: '二级 1-1' },
-                        { label: '三级 1-1-1' },
-                        { label: '三级 1-1-1' },
-                        { label: '三级 1-1-1' }
-                    ]
-                },
-                {
-                    label: '一级 2',
-                    children: [
-                        { label: '二级 1-1' },
-                        { label: '三级 1-1-1' },
-                        { label: '三级 1-1-1' },
-                        { label: '三级 1-1-1' }
-                    ]
-                },
-                {
-                    label: '一级 3',
-                    children: [
-                        { label: '二级 1-1' },
-                        { label: '三级 1-1-1' },
-                        { label: '三级 1-1-1' },
-                        { label: '三级 1-1-1' }
-                    ]
-                }],
-            defaultProps: {
-                children: 'children',
-                label: 'label'
-            }
-        };
-    },
-    methods: {
-        handleNodeMove(data, node, e) {
-            this.$emit('mousedown', data, e)
+  data() {
+    return {
+      data: [
+        {
+          normal_field: "一级 1",
+          children: [
+            { normal_field: "二级 1-1" },
+            { normal_field: "三级 1-1-1" },
+            { normal_field: "三级 1-1-1" },
+            { normal_field: "三级 1-1-1" },
+          ],
         },
-        onDragstart(e){
-            console.log(e)
-        }
-    }
-}
+        {
+          normal_field: "一级 2",
+          children: [
+            { normal_field: "二级 1-1" },
+            { normal_field: "三级 1-1-1" },
+            { normal_field: "三级 1-1-1" },
+            { normal_field: "三级 1-1-1" },
+          ],
+        },
+        {
+          normal_field: "一级 3",
+          children: [
+            { normal_field: "二级 1-1" },
+            { normal_field: "三级 1-1-1" },
+            { normal_field: "三级 1-1-1" },
+            { normal_field: "三级 1-1-1" },
+          ],
+        },
+      ],
+      defaultProps: {
+        children: "children",
+        label: "normal_field",
+      },
+    };
+  },
+  methods: {
+    handleNodeMove(data, node, e) {
+      this.$emit("mousedown", data, e);
+    },
+    onDragstart(e, data) {
+      data.PK_field = "xxx";
+      let dataStr = JSON.stringify(data);
+      e.dataTransfer.setData("data-info", dataStr);
+    },
+  },
+};
 </script>
 
 <style scoped>
 .move {
-    cursor: move;
+  cursor: move;
 }
 </style>
